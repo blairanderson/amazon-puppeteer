52f70c03961c93d675fa21954db53a6474a508af
Blair Anderson <Blair81@gmail.com>
2017-09-07 15:26:45 -0700

    fix the bug with lots of reviews

--- parsers/reviews.js
+++ parsers/reviews.js
@@ -1,7 +1,7 @@
 module.exports = {};
 module.exports.parse = function($) {
   const rating = $('#acrPopover').attr('title');
-  const ratingAverage = parseFloat(rating);
+  const ratingAverage = parseFloat(rating.replace(/,/g, ''));
   // for some reason this is duplicated.
   const textRaw = $('#acrCustomerReviewLink #acrCustomerReviewText')
     .text()
@@ -9,7 +9,7 @@ module.exports.parse = function($) {
 
   // "675 customer reviews"
   const text = textRaw.slice(0, textRaw.length / 2);
-  const count = parseInt(text);
+  const count = parseInt(text.replace(/,/g, ''));
 
   return {
     text,
