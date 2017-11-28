60f5af736f3e9a96804b066217fdd50fbad4bd95
Blair Anderson <Blair81@gmail.com>
2017-11-27 16:43:13 -0800

    add search function

--- fetchquery.js
+++ fetchquery.js
@@ -1,5 +1,28 @@
 const puppeteer = require('puppeteer');
-const parse = require('amazon-html-to-json').parse;
+const querystring = require('querystring');
+const fs = require('fs');
+const cheerio = require('cheerio');
+const parseQuery = function(html, asin) {
+  const $ = cheerio.load(html);
+  const results = $('#s-results-list-atf li')
+    .map(function(index) {
+      return {
+        rank: index + 1,
+        asin: $(this).data('asin'),
+        matched: $(this).data('asin') === asin,
+        title: $(this)
+          .find('h2')
+          .text()
+          .trim(),
+        image: $(this)
+          .find('img')
+          .attr('src')
+          .trim()
+      };
+    })
+    .get();
+  return { results };
+};
 
 const userAgent =
   'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36';
@@ -21,15 +44,18 @@ const log = (...args) => {
   }
 };
 
-async function fetchASIN(asin) {
+async function fetchQuery(keywords, asin) {
   const browser = await puppeteer.launch(puppeteerArgs);
   const page = await browser.newPage();
   page.on('console', (...args) => console.log('PAGE LOG:', ...args));
   let screenshot = false;
   let images = false;
   console.log(asin);
-  const path = `https://www.amazon.com/dp/${asin}`;
-  const newData = { asin, path, timeNow: Date().toString() };
+  const keyquerystring = querystring.stringify({ keywords });
+  const path = `https://www.amazon.com/s?${
+    keyquerystring
+  }&unfiltered=1&ie=UTF8`;
+  const newData = { asin, keywords, path, timeNow: Date().toString() };
   await page.setViewport({ width: 1680, height: 895 });
   log('viewport');
   await page.setUserAgent(userAgent);
@@ -48,45 +74,11 @@ async function fetchASIN(asin) {
     return { error };
   }
 
-  if (process.env.SCREENSHOT === 'true' || process.env.SCREENSHOT === true) {
-    try {
-      log('screenshot');
-      await page.screenshot({ path: `tmp/${asin}.png`, fullPage: true });
-      newData['screenshot'] = `${host}/img/${asin}.png`;
-      log('screenshot:', newData['screenshot']);
-    } catch (error) {
-      log('screenshot error');
-      browser.close();
-      return { error };
-    }
-  }
-
-  function fetchImages() {
-    return new Promise((resolve, reject) => {
-      if (window.P) {
-        P.when('ImageBlockATF').execute(function(dat) {
-          if (dat) {
-            resolve(dat);
-          } else {
-            reject('nope');
-          }
-        });
-      } else {
-        reject('nope');
-      }
-    });
-  }
-
-  try {
-    images = await page.evaluate(fetchImages);
-  } catch (e) {
-    console.log(e);
-  }
   try {
     const content = await page.content();
     log('content downloaded');
     log('parsing started', Date().toString());
-    const parsed = parse(content, images);
+    const parsed = parseQuery(content, asin);
     log('content parsed', Date().toString());
     browser.close();
     return Object.assign(newData, parsed);
@@ -97,4 +89,4 @@ async function fetchASIN(asin) {
   }
 }
 
-module.exports = { fetchASIN };
+module.exports = { fetchQuery };
--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
-const fetchASIN = require('./fetchasin.js');
+const { fetchASIN } = require('./fetchasin.js');
+const { fetchQuery } = require('./fetchquery.js');
 const start = require('./utils').start;
 const rd = require('redis').createClient;
 const redis = process.env.REDIS_URL
@@ -14,18 +15,22 @@ app.use(require('./utils').ignoreFavicon);
 
 app.get('/', function(req, res) {
   res.status(200).json({
-    routes: ['/:asin?api_key=poopoo']
+    routes: [
+      '/s?keywords=something&asin=BOOFOO1234&api_key=poopoo',
+      '/:asin?api_key=poopoo'
+    ]
   });
 });
 
 app.use('/img', express.static('tmp'));
 app.use(require('express-middleware-apikey')(process.env.API_KEY.split(',')));
-app.get('/:asin', cache, asinController);
+app.get('/s', queryCache, queryController);
+app.get('/:asin', asinCache, asinController);
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
-function cache(req, res, next) {
+function asinCache(req, res, next) {
   if (process.env.CACHE === 'true' || process.env.CACHE === true) {
     const { asin } = req.params;
     const timer = start();
@@ -43,6 +48,27 @@ function cache(req, res, next) {
   } else {
     next();
   }
+}
+
+function queryCache(req, res, next) {
+  return next();
+  // if (process.env.CACHE === 'true' || process.env.CACHE === true) {
+  //   const { asin } = req.params;
+  //   const timer = start();
+  //   redis.get(asin, function(err, data) {
+  //     if (err) throw err;
+  //
+  //     if (data != null) {
+  //       console.log('cache-hit', asin);
+  //       res.json(Object.assign({ time: timer.done() }, JSON.parse(data)));
+  //     } else {
+  //       console.log('cache-miss', asin);
+  //       next();
+  //     }
+  //   });
+  // } else {
+  //   next();
+  // }
 }
 
 function asinController(req, res) {
@@ -71,6 +97,38 @@ function asinController(req, res) {
       res,
       'did not pass regex',
       'are you sure this passes ([A-Z0-9]{10})'
+    );
+  }
+}
+
+function queryController(req, res) {
+  const timer = start();
+  const { keywords, asin } = req.query;
+  const valid =
+    keywords.split(' ').length > 1 && asin.match(new RegExp('([A-Z0-9]{10})'));
+
+  if (valid) {
+    fetchQuery(keywords, asin)
+      .then(function(data) {
+        // seconds * minutes * hours
+        const exp = 60 * 60 * 3; // 6 hours
+        if (!data.error) {
+          redis.setex(keywords, exp, JSON.stringify(data));
+        }
+        res.json(Object.assign({ time: timer.done() }, data));
+      })
+      .catch(function(error) {
+        timer.done();
+        console.log('NOT FOUND', JSON.stringify(error));
+        notFound(req, res, 'Caught in fetch', error);
+      });
+  } else {
+    timer.done();
+    notFound(
+      req,
+      res,
+      'Must include keywords AND asin.',
+      'Are you sure this passes ([A-Z0-9]{10})'
     );
   }
 }
