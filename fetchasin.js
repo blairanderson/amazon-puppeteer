26fb773c3467678837fec7312d73abcaee77f9e3
Blair Anderson <Blair81@gmail.com>
2018-06-05 11:54:57 -0700

    try this humdinger

--- fetchasin.js
+++ fetchasin.js
@@ -1,30 +1,30 @@
-const puppeteer = require('puppeteer');
-const cheerio = require('cheerio');
-const { puppeteerArgs, userAgent, host, log } = require('./puppeteerargs.js');
+const puppeteer = require("puppeteer");
+const cheerio = require("cheerio");
+const { puppeteerArgs, userAgent, host, log } = require("./puppeteerargs.js");
 const expiration = 60 * 60 * 24;
-const rd = require('redis').createClient;
+const rd = require("redis").createClient;
 const redis = process.env.REDIS_URL
   ? rd(process.env.REDIS_URL)
-  : rd(6379, 'localhost');
-const parse = require('amazon-html-to-json').parse;
+  : rd(6379, "localhost");
+const parse = require("amazon-html-to-json").parse;
 
 async function fetchASIN(asin) {
   const browser = await puppeteer.launch({
     ignoreHTTPSErrors: true,
     args: [
-      '--proxy-server=proxy.crawlera.com:8010',
-      '--no-sandbox',
-      '--disable-setuid-sandbox'
+      "--proxy-server=proxy.crawlera.com:8010",
+      "--no-sandbox",
+      "--disable-setuid-sandbox"
     ]
   });
   const page = await browser.newPage();
   await page.setExtraHTTPHeaders({
-    'Proxy-Authorization':
-      'Basic ' +
-      Buffer.from(`${process.env.CRAWLERA_APIKEY}:`).toString('base64')
+    "Proxy-Authorization":
+      "Basic " +
+      Buffer.from(`${process.env.CRAWLERA_APIKEY}:`).toString("base64")
   });
 
-  page.on('console', (...args) => console.log('PAGE LOG:', ...args));
+  page.on("console", (...args) => console.log("PAGE LOG:", ...args));
   let screenshot = false;
   let images = false;
   console.log(asin);
@@ -32,55 +32,61 @@ async function fetchASIN(asin) {
   const path = `https://www.amazon.com/dp/${asin}`;
   const newData = { asin, path, timeNow: Date().toString() };
   await page.setViewport({ width: 1680, height: 895 });
-  log('viewport');
+  log("viewport");
   await page.setUserAgent(userAgent);
-  log('useragent', userAgent);
+  log("useragent", userAgent);
 
   try {
-    console.log('path', path);
+    console.log("path", path);
     var start = +new Date();
     var resp = await page.goto(path, {
       timeout: 0,
-      waitUntil: 'domcontentloaded'
+      waitUntil: "domcontentloaded"
     });
+    newData["status"] = resp.status();
     var end = +new Date();
-    console.log('start-end-diff', (end - start) / 1000);
+    console.log("start-end-diff", (end - start) / 1000);
+    log("goto", path);
     if (!resp.ok) {
       browser.close();
-      return { status: resp.status, error: `ASIN NOT OK. ${resp.status}` };
+      return { status: resp.status(), error: `ASIN NOT OK. ${resp.status()}` };
     }
-    log('goto', path);
+
+    if (!resp.ok()) {
+      browser.close();
+      return { status: resp.status(), error: `ASIN NOT OK. ${resp.status()}` };
+    }
   } catch (error) {
-    log('page.goto ERROR', JSON.stringify(error));
+    log("page.goto ERROR", JSON.stringify(error));
     browser.close();
-    return { error: error.toString(), stack: error.stack.split('\n') };
+    return { error: error.toString(), stack: error.stack.split("\n") };
   }
 
-  if (process.env.SCREENSHOT === 'true' || process.env.SCREENSHOT === true) {
+  if (process.env.SCREENSHOT === "true" || process.env.SCREENSHOT === true) {
     try {
-      log('screenshot');
+      log("screenshot");
       await page.screenshot({ path: `tmp/${asin}.png`, fullPage: true });
-      newData['screenshot'] = `${host}/img/${asin}.png`;
-      log('screenshot:', newData['screenshot']);
+      newData["screenshot"] = `${host}/img/${asin}.png`;
+      log("screenshot:", newData["screenshot"]);
     } catch (error) {
-      log('screenshot error');
+      log("screenshot error");
       browser.close();
-      return { error: error.toString(), stack: error.stack.split('\n') };
+      return { error: error.toString(), stack: error.stack.split("\n") };
     }
   }
 
   function fetchImages() {
     return new Promise((resolve, reject) => {
       if (window.P) {
-        P.when('ImageBlockATF').execute(function(dat) {
+        P.when("ImageBlockATF").execute(function(dat) {
           if (dat) {
             resolve(dat);
           } else {
-            reject('nope');
+            reject("nope");
           }
         });
       } else {
-        reject('nope');
+        reject("nope");
       }
     });
   }
@@ -93,19 +99,19 @@ async function fetchASIN(asin) {
 
   try {
     const content = await page.content();
-    log('content downloaded');
-    log('parsing started', Date().toString());
+    log("content downloaded");
+    log("parsing started", Date().toString());
     const $ = cheerio.load(content);
     const parsed = parse($, images);
-    log('content parsed', Date().toString());
+    log("content parsed", Date().toString());
     browser.close();
     const parsedData = Object.assign(newData, parsed);
     redis.setex(asin, expiration, JSON.stringify(parsedData));
     return parsedData;
   } catch (error) {
     browser.close();
-    console.log('ERR', error.toString(), error.stack);
-    return { error: error.toString(), stack: error.stack.split('\n') };
+    console.log("ERR", error.toString(), error.stack);
+    return { error: error.toString(), stack: error.stack.split("\n") };
   }
 }
 
--- index.js
+++ index.js
@@ -1,55 +1,53 @@
-require('dotenv').config();
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const { fetchASIN } = require('./fetchasin.js');
-const { fetchQuery } = require('./fetchquery.js');
-const start = require('./utils').start;
+const { fetchASIN } = require("./fetchasin.js");
+const { fetchQuery } = require("./fetchquery.js");
+const { DOCS } = require("./lib/docs.js");
 
-const rd = require('redis').createClient;
+const start = require("./utils").start;
+
+const rd = require("redis").createClient;
 const redis = process.env.REDIS_URL
   ? rd(process.env.REDIS_URL)
-  : rd(6379, 'localhost');
+  : rd(6379, "localhost");
 
-app.set('port', process.env.PORT || 5000);
-app.use(express.static(__dirname + '/public'));
-app.use(require('./utils').ignoreFavicon);
-
-app.get('/', function(req, res) {
-  res.status(200).json({
-    routes: [
-      '/s?keywords=something&asin=BOOFOO1234&api_key=poopoo',
-      '/:asin?api_key=poopoo'
-    ]
-  });
-});
-
-app.use('/img', express.static('tmp'));
+app.set("port", process.env.PORT || 5000);
+app.use(express.static(__dirname + "/public"));
+app.use(require("./utils").ignoreFavicon);
+app.get("/", DOCS);
+app.use("/img", express.static("tmp"));
 app.use(
-  require('express-middleware-apikey')(process.env.PUPP_API_KEY.split(','))
+  require("express-middleware-apikey")(process.env.PUPP_API_KEY.split(","))
 );
-app.get('/s', queryCache, queryController);
-app.get('/:asin', asinCache, asinController);
-app.listen(app.get('port'), function() {
-  console.log('Node app is running on port', app.get('port'));
+
+// NOT QUITE USED BUT WANT TO REALLY BADLY
+app.get("/s", queryCache, queryController);
+
+// THIS IS THE CATS MEOW
+app.get("/:asin", asinCache, asinController);
+
+app.listen(app.get("port"), function() {
+  console.log("Node app is running on port", app.get("port"));
 });
 
 function asinCache(req, res, next) {
-  if (process.env.CACHE === 'true' || process.env.CACHE === true) {
-    console.log('cache');
+  if (process.env.CACHE === "true" || process.env.CACHE === true) {
+    console.log("cache");
     const { asin } = req.params;
     const timer = start();
     redis.get(asin, function(err, data) {
       if (err) throw err;
       if (data != null) {
-        console.log('cache-hit', asin);
+        console.log("cache-hit", asin);
         res.json(Object.assign({ time: timer.done() }, JSON.parse(data)));
       } else {
-        console.log('cache-miss', asin);
+        console.log("cache-miss", asin);
         next();
       }
     });
   } else {
-    console.log('no-cache');
+    console.log("no-cache");
     next();
   }
 }
@@ -81,30 +79,30 @@ function succcr(data) {
   console.log(JSON.stringify(data, null, 4));
 }
 function errrrno(error) {
-  console.log(error.stack.split('\n'));
+  console.log(error.stack.split("\n"));
 }
 // fetchASIN('B008E338FY').then(succcr).catch(errrrno);
 
 function asinController(req, res) {
   const timer = start();
   const { asin } = req.params;
-  if (asin.match(new RegExp('([A-Z0-9]{10})'))) {
+  if (asin.match(new RegExp("([A-Z0-9]{10})"))) {
     fetchASIN(asin)
       .then(function(data) {
         res.json(Object.assign({ time: timer.done() }, data));
       })
       .catch(function(error) {
         timer.done();
-        console.log('NOT FOUND', error.stack.split('\n'));
-        notFound(req, res, 'Caught in fetch', error.stack.split('\n'));
+        console.log("NOT FOUND", error.stack.split("\n"));
+        notFound(req, res, "Caught in fetch", error.stack.split("\n"));
       });
   } else {
     timer.done();
     notFound(
       req,
       res,
-      'did not pass regex',
-      'are you sure this passes ([A-Z0-9]{10})'
+      "did not pass regex",
+      "are you sure this passes ([A-Z0-9]{10})"
     );
   }
 }
@@ -113,7 +111,7 @@ function queryController(req, res) {
   const timer = start();
   const { keywords, asin } = req.query;
   const valid =
-    keywords.split(' ').length > 1 && asin.match(new RegExp('([A-Z0-9]{10})'));
+    keywords.split(" ").length > 1 && asin.match(new RegExp("([A-Z0-9]{10})"));
 
   if (valid) {
     fetchQuery(keywords, asin)
@@ -127,16 +125,16 @@ function queryController(req, res) {
       })
       .catch(function(error) {
         timer.done();
-        console.log('NOT FOUND', JSON.stringify(error));
-        notFound(req, res, 'Caught in fetch', error);
+        console.log("NOT FOUND", JSON.stringify(error));
+        notFound(req, res, "Caught in fetch", error);
       });
   } else {
     timer.done();
     notFound(
       req,
       res,
-      'Query must include keywords AND asin.',
-      'Are you sure this passes ([A-Z0-9]{10})'
+      "Query must include keywords AND asin.",
+      "Are you sure this passes ([A-Z0-9]{10})"
     );
   }
 }
